d80573f785481222e7687d6bf56c2c9d9932f29e
Robert Cramer <robert.cramer@callrail.com>
2021-04-30 16:40:25 -0400

    implement create method and write request specs for user exams

--- app/controllers/user_exams_controller.rb
+++ app/controllers/user_exams_controller.rb
@@ -1,4 +1,49 @@
 class UserExamsController < ApplicationController
+  before_action :set_college
+  before_action :set_exam
+  before_action :check_valid_start
+
   def create
+    begin
+      user = User.find_or_create_by(request_params.slice(:first_name, :last_name, :phone_number))
+      user_exam = UserExam.create(
+        start_time: DateTime.parse(request_params[:start_time]),
+        user_id: user.id,
+        exam_id: @exam.id
+      )
+      render json: {message: 'User exam created'}, status: 200
+    rescue => error
+      bad_request('Error creating user exam')
+    end
+  end
+
+  private
+
+  def request_params
+    params.require(:user_exam).permit(:first_name, :last_name, :phone_number, :college_id, :exam_id, :start_time)
+  end
+
+  def set_college
+    @college = College.find(request_params[:college_id])
+    rescue ActiveRecord::RecordNotFound
+      bad_request('College not found')
+  end
+
+  def set_exam
+    @exam = @college.exams.find(request_params[:exam_id])
+    rescue ActiveRecord::RecordNotFound
+      bad_request('Exam not found')
+  end
+
+  def check_valid_start
+    valid_time = DateTime.parse(request_params[:start_time]).between?(@exam.start_time, @exam.end_time)
+    bad_request('Invalid start time') unless valid_time
+  end
+
+  def bad_request(message)
+    payload = {
+      error: message
+    }
+    render json: payload, status: :bad_request
   end
 end
--- spec/requests/user_exams_spec.rb
+++ spec/requests/user_exams_spec.rb
@@ -2,6 +2,61 @@ require 'rails_helper'
 
 RSpec.describe "UserExams", type: :request do
   describe "POST /user_exams" do
-    pending "add some examples (or delete) #{__FILE__}"
+    let(:exam) { FactoryBot.create(:exam) }
+    let(:user) { FactoryBot.create(:user) }
+    let(:params) {
+      {
+        first_name: user.first_name,
+        last_name: user.last_name,
+        phone_number: user.phone_number,
+        college_id: exam.college_id,
+        exam_id: exam.id,
+        start_time: exam.start_time + 10.minutes
+      }
+    }
+
+    subject { post '/user_exams', params: { user_exam: params } }
+
+    context 'when unsuccessful' do
+      it 'returns 400 when no college is found' do
+        params[:college_id] = nil
+        subject
+        expect(response).to have_http_status(400)
+        expect(JSON.parse(response.body)['error']).to eq('College not found')
+      end
+
+      it 'returns 400 when no exam is found' do
+        params[:exam_id] = nil
+        subject
+        expect(response).to have_http_status(400)
+        expect(JSON.parse(response.body)['error']).to eq('Exam not found')
+      end
+
+      it 'returns 400 when start_time is invalid' do
+        params[:start_time] = exam.start_time - 10.minutes
+        subject
+        expect(response).to have_http_status(400)
+        expect(JSON.parse(response.body)['error']).to eq('Invalid start time')
+      end
+
+      it 'returns 400 when user find or create fails' do
+        allow_any_instance_of(ActiveRecord::Relation).to receive(:find_or_create_by).and_raise('boom')
+        subject
+        expect(response).to have_http_status(400)
+        expect(JSON.parse(response.body)['error']).to eq('Error creating user exam')
+      end
+    end
+
+    context 'when successful' do
+      it 'returns a 200 success' do
+        subject
+        expect(response).to have_http_status(200)
+        expect(JSON.parse(response.body)['message']).to eq('User exam created')
+      end
+
+      it 'creates a user_exam' do
+        expect { subject }.to change { UserExam.count }.from(0).to(1)
+      end
+    end
   end
 end
