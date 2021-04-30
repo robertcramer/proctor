2b174a87857a367d9390928e11f3e4532dd36532
Robert Cramer <robert.cramer@callrail.com>
2021-04-30 16:43:59 -0400

    create some basic seed data using factories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Create some Exams which will create Colleges too w/ no user_exams yet
+10.times do
+  FactoryBot.create(:exam)
+end
+
+# Create some Users with no user exams yet
+10.times do
+  FactoryBot.create(:user)
+end
+
+# Create User Exams which will create users, exams, and colleges
+10.times do
+  FactoryBot.create(:user_exam)
+end
